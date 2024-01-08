1790f375bc9f2828d651b45dade88dda71247468
Davis Hedgepeth <dnh@umbraco.dk>
2024-01-08 10:13:30 -0500

    load balancing server roles

---
+++ V13LoadBalance.Web/ServerRoleAccessors/SingleServerRoleAccessor.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Cms.Core.Sync;
+
+namespace V13LoadBalance.Web.ServerRoleAccessors
+{
+    public class SingleServerRoleAccessor : IServerRoleAccessor
+    {
+        public ServerRole CurrentServerRole => ServerRole.Single;
+    }
+}
---
+++ V13LoadBalance.Web/ServerRoleAccessors/SubscriberServerRoleAccessor.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Cms.Core.Sync;
+
+namespace V13LoadBalance.Web.ServerRoleAccessors
+{
+    public class SubscriberServerRoleAccessor : IServerRoleAccessor
+    {
+        public ServerRole CurrentServerRole => ServerRole.Subscriber;
+    }
+}
