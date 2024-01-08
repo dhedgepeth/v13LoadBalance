907c309e66ddd323875a6259c1179e8ec47acdbd
Umbraco Cloud <support@umbraco.io>
2024-01-08 15:18:11 +0000

    Deployment 25edc131-1345-4c8a-9614-a0c362f1114c: Run number 3

---
+++ V13LoadBalance.Web/ServerRoleAccessors/SchedulingPublisherServerRoleAccessor.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Cms.Core.Sync;
+
+namespace V13LoadBalance.Web.ServerRoleAccessors
+{
+    public class SchedulingPublisherServerRoleAccessor : IServerRoleAccessor
+    {
+        public ServerRole CurrentServerRole => ServerRole.SchedulingPublisher;
+    }
+}
