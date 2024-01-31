ea016d22aac859181f74f70200e2d5c848651838
Umbraco Cloud <support@umbraco.io>
2024-01-31 17:25:14 +0000

    Deployment d70959be-c443-47a1-9549-ba2cf464e0f8: Run number 30

---
+++ V13LoadBalance.Web/Indexes/TVshowPageIndex.cs
@@ -0,0 +1,24 @@
+﻿using Examine.Lucene;
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Infrastructure.Examine;
+
+namespace V13LoadBalance.Web.Indexes
+{
+    public class TVshowPageIndex : UmbracoExamineIndex
+    {
+        public TVshowPageIndex(
+            ILoggerFactory loggerFactory, 
+            string name, 
+            IOptionsMonitor<LuceneDirectoryIndexOptions> indexOptions, 
+            Umbraco.Cms.Core.Hosting.IHostingEnvironment hostingEnvironment, 
+            IRuntimeState runtimeState)
+            : base(loggerFactory, 
+                  name, 
+                  indexOptions, 
+                  hostingEnvironment, 
+                  runtimeState)
+        {
+        }
+    }
+}
---
+++ V13LoadBalance.Web/Indexes/TVshowPageIndexPopulator.cs
@@ -0,0 +1,45 @@
+﻿using Examine;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Infrastructure.Examine;
+
+namespace V13LoadBalance.Web.Indexes
+{
+    public class TVshowPageIndexPopulator : IndexPopulator
+    {
+        private readonly IContentService _contentService;
+        private readonly TVshowPageIndexValueSetBuilder _tvShowPageIndexValueSetBuilder;
+
+        public TVshowPageIndexPopulator(IContentService contentService, TVshowPageIndexValueSetBuilder tvShowPageIndexValueSetBuilder)
+        {
+            _contentService = contentService;
+            _tvShowPageIndexValueSetBuilder = tvShowPageIndexValueSetBuilder;
+            RegisterIndex("TVshowPageIndex");
+        }
+
+        protected override void PopulateIndexes(IReadOnlyList<IIndex> indexes)
+        {
+            foreach (IIndex index in indexes)
+            {
+                IContent[] roots = _contentService.GetRootContent().ToArray();
+                index.IndexItems(_tvShowPageIndexValueSetBuilder.GetValueSets(roots));
+
+                foreach (IContent root in roots)
+                {
+                    const int pageSize = 10000;
+                    var pageIndex = 0;
+                    IContent[] descendants;
+                    do
+                    {
+                        descendants = _contentService.GetPagedDescendants(root.Id, pageIndex, pageSize, out _).ToArray();
+                        IEnumerable<ValueSet> valueSets = _tvShowPageIndexValueSetBuilder.GetValueSets(descendants);
+                        index.IndexItems(valueSets);
+
+                        pageIndex++;
+                    }
+                    while (descendants.Length == pageSize);
+                }
+            }
+        }
+    }
+}
